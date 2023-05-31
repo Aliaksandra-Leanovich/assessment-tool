24606df91c13c136b0f2dccdce5cf222eb255ae6
aliaksandraleanovich <a.lenovich@softteco.com>
2023-05-31 14:47:03 +0300

    feat: add eror code descriptions

--- client/src/helper/authErrors.ts
+++ client/src/helper/authErrors.ts
@@ -14,6 +14,16 @@ export const getAuthError = (error: string) => {
       return "Operation not allowed.";
     case "auth/weak-password":
       return "The password is too weak.";
+    case "auth/credential-already-in-use":
+      return "This credential is already associated with a different user account.";
+    case "auth/user-token-expired":
+      return "The user's credential is no longer valid. The user must sign in again.";
+    case "auth/popup-blocked":
+      return "Unable to establish a connection with the popup. It may have been blocked by the browser.";
+    case "auth/popup-closed-by-user":
+      return "The popup has been closed by the user before finalizing the operation.";
+    case "auth/provider-already-linked":
+      return "User can only be linked to one identity for the given provider.";
     default:
       return "An unexpected error occurred.";
   }
