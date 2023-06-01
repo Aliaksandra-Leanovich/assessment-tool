3a27b59b15a08edf44429ebfdd4a819f11ef221b
aliaksandraleanovich <a.lenovich@softteco.com>
2023-06-01 14:16:53 +0300

    feat: add outsideclick for modal

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import { ReactComponent as CrossIcon } from "../../assets/cross-icon.svg";
+import { useOutsideClick } from "../../hooks";
 import {
   ButtonSC,
   ContainerButtonSC,
@@ -9,9 +11,16 @@ import {
 import { IModalProps } from "./types";
 
 export const Modal = ({ handleClose, show, children }: IModalProps) => {
+  const selectRef = useRef(null);
+
+  useOutsideClick(selectRef, () => {
+    if (show) {
+      handleClose();
+    }
+  });
   return (
-    <WrapperSC show={show}>
-      <ContainerSC show={show}>
+    <WrapperSC show={show} >
+      <ContainerSC show={show} ref={selectRef}>
         <ContainerButtonSC>
           <ButtonSC type="button" onClick={handleClose}>
             <CrossIcon />
--- client/src/hooks/use-outside-click.hook.ts
+++ client/src/hooks/use-outside-click.hook.ts
@@ -1,16 +1,19 @@
-import { RefObject, useEffect } from "react";
+import { RefObject, useCallback, useEffect } from "react";
 
 export const useOutsideClick = (
   ref: RefObject<HTMLInputElement>,
   callback: (event: MouseEvent) => void
 ) => {
-  const listener = (event: MouseEvent) => {
-    if (!ref.current || ref.current.contains(event.target as Node)) return;
-    callback(event);
-  };
+  const listener = useCallback(
+    (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) return;
+      callback(event);
+    },
+    [callback, ref]
+  );
 
   useEffect(() => {
     document.addEventListener("click", listener);
     return () => document.removeEventListener("click", listener);
-  }, []);
+  }, [listener]);
 };
